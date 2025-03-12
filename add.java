c1348b3e7f4af53accb5c2682140cd1c6ff62c56
anjanarajuu <anjanakallumurickal@gmail.com>
2025-03-12 14:14:39 +0530

    JAVA-MODULE1-PRGRM

---
+++ add.java
@@ -0,0 +1,11 @@
+public class add
+{
+ public static void main(String[] args)
+ {
+ int x=10;
+ int y=25;
+ int sum=x+y;
+ System.out.println(sum);
+ }
+ }
+ 
---
+++ demo.java
@@ -0,0 +1,12 @@
+public class demo
+{
+public static void main(String[]args)
+{
+int num=10;
+if(num>0)
+{
+System.out.println("number is +ve");
+}
+}
+}
+
